a26c75ccdc217f2d55c9037613db68172455b606
Jorgen Lovbakke <jorgenlovbakke@yahoo.co.uk>
2024-01-07 18:45:26 +0000

    Add refreshed setting.py and env.py files

--- communityincidentforum/settings.py
+++ communityincidentforum/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-jl14-communityinciden-fuudqhlrtxk.ws-eu107.gitpod.io',
+ALLOWED_HOSTS = ['8000-jl14-communityinciden-mdkyok2sobs.ws-eu107.gitpod.io',
 'community-incident-forum-75e7791f6b68.herokuapp.com', 
 'localhost',]
 
