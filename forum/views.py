1b1ff7103272a0f87538917142906f38e12b0904
Jorgen Lovbakke <130441202+JL-14@users.noreply.github.com>
2024-02-20 17:42:06 +0000

    Add updated url configuration

--- forum/views.py
+++ forum/views.py
@@ -5,6 +5,6 @@ from .models import Issue
 
 class ReportList (generic.ListView):
     model = Issue
-    queryset = Issue.objects.filter(approved = 1).order_by('-date_of_issue')
-    template_name = 'index.html'
-    paginate_by = 6
+    # queryset = Issue.objects.filter(approved = 1).order_by('-date_of_issue')
+    # template_name = 'index.html'
+    # paginate_by = 6
