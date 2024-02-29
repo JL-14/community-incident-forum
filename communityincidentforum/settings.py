5a2638772aba0e6dfb8e78a5343c7bb16d9b1349
Jorgen Lovbakke <jorgenlovbakke@yahoo.co.uk>
2024-02-29 21:34:35 +0000

    Add with correct settings.py and admin.py for database

--- communityincidentforum/settings.py
+++ communityincidentforum/settings.py
@@ -35,13 +35,13 @@ ALLOWED_HOSTS = ['8000-jl14-communityinciden-mdkyok2sobs.ws-eu107.gitpod.io',
 'localhost',
 '8000-jl14-communityinciden-hx10yg5oxjv.ws-eu108.gitpod.io',
 '8000-jl14-communityinciden-rtz5trbpxri.ws-eu108.gitpod.io',
+ '8000-jl14-communityinciden-32dcrhqwhbp.ws-eu108.gitpod.io',
+'*.gitpod.io',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-jl14-communityinciden-fuudqhlrtxk.ws-eu107.gitpod.io',
+    'https://*.gitpod.io',
     'https://*.herokuapp.com',
-    'https://8000-jl14-communityinciden-hx10yg5oxjv.ws-eu108.gitpod.io',
-    'https://8000-jl14-communityinciden-rtz5trbpxri.ws-eu108.gitpod.io',
 ]
 
 # Application definition
--- forum/admin.py
+++ forum/admin.py
@@ -2,15 +2,6 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import UserAccount, DeptNotified, Issue
 
-@admin.register(Issue)
-class TextAdmin(SummernoteModelAdmin):
-    """Using Summernote formatting for admin page"""
-    list_display = ('date_of_issue', 'issue_title', 'issue_type', 'issue_location')
-    search_fields = ['issue_title', 'issue_location']
-    list_filter = ('issue_type', 'date_of_issue', 'status')
-    prepopulated_fields = {'slug': ('issue_title',)}
-    readonly_fields = ['slug',]
-    summernote_fields = ('issue_content', 'notes_about_notifications')
-
+admin.site.register(Issue)
 admin.site.register(UserAccount)
 admin.site.register(DeptNotified)
