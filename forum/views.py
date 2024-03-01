3bf3a42d73fbda0d4a5f664455dfaf4690b91c68
Jorgen Lovbakke <130441202+JL-14@users.noreply.github.com>
2024-03-01 11:52:16 +0000

    Add report_detail.html template

--- forum/views.py
+++ forum/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Issue
 
 
 class IssueList (generic.ListView):
-    # model = Issue
     queryset = Issue.objects.order_by('-date_of_issue')
-    # template_name = 'issue_list.html'
     template_name = 'forum/index.html'
     paginate_by = 6
+
+def report_detail(request, slug):
+    """
+    Display an individual :model:`forum.Issue`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`forum.Issue`.
+
+    **Template:**
+
+    :template:`forum/report_detail.html`
+    """
+
+    queryset = Issue.objects.filter(approved=1)
+    report = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "forum/report_detail.html",
+        {"issue": report},
+    )
