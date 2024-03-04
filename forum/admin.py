6f689d2f5635f2311767b75a57512067ce30eef2
Jorgen Lovbakke <130441202+JL-14@users.noreply.github.com>
2024-03-04 14:22:19 +0000

    Add list of views in admin.py

--- forum/admin.py
+++ forum/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import UserAccount, DeptNotified, Issue
 
-admin.site.register(Issue)
+@admin.register(Issue)
+class IssueDetails(SummernoteModelAdmin):
+    list_display = ('issue_title', 'slug', 'approved', 'date_of_issue', 'issue_location', 'user', 'created_on')
+    search_fields = ['issue_title', 'issue_content', 'issue_location']
+    list_filter = ('date_of_issue', 'approved', 'status', 'issue_type')
+    prepopulated_fields = {'slug':('issue_title',)}
+    summernote_fields = ('issue_content',)
+
 admin.site.register(UserAccount)
 admin.site.register(DeptNotified)
